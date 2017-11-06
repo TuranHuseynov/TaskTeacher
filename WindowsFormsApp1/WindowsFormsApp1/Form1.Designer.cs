288dcb90745dff811555da0d9975773094ff497a
TuranHuseynov <turaneh@code.edu.az>
2017-11-06 17:58:20 +0400

    task

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TaskBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TeacherNameBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TaskBox
+            // 
+            this.TaskBox.FormattingEnabled = true;
+            this.TaskBox.Location = new System.Drawing.Point(25, 367);
+            this.TaskBox.Name = "TaskBox";
+            this.TaskBox.Size = new System.Drawing.Size(165, 21);
+            this.TaskBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Elave Etmek Istediyiniz Task-i sechin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Muellimin Adi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 1;
+            // 
+            // TeacherNameBox
+            // 
+            this.TeacherNameBox.FormattingEnabled = true;
+            this.TeacherNameBox.Location = new System.Drawing.Point(29, 69);
+            this.TeacherNameBox.Name = "TeacherNameBox";
+            this.TeacherNameBox.Size = new System.Drawing.Size(165, 21);
+            this.TeacherNameBox.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Telebenin Adi";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(25, 255);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(165, 21);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1349, 645);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.TeacherNameBox);
+            this.Controls.Add(this.TaskBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox TaskBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox TeacherNameBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            var conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\P104\Documents\Task.mdf;Integrated Security=True;Connect Timeout=30";
+            var query = "select * from Teacher";
+            var obj = new Task();
+            obj.Connection(conString);
+            obj.Command(query);
+            obj.Reader();
+            foreach (var item in Task.myList)
+            {
+                TeacherNameBox.Items.Add(item);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < Task.taskName.Count; i++)
+            {
+                Task.taskName.Remove(Task.taskName[i]);
+                
+            
+            }
+          
+            var conString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\P104\Documents\Task.mdf;Integrated Security=True;Connect Timeout=30";
+            var query = "select Id from Teacher where name='"+TeacherNameBox.SelectedItem+"'";
+            int a;
+            var obj = new Task();
+            obj.Connection(conString);
+            obj.Command(query);
+            obj.Rdr();
+
+            for (int i = 0; i < Task.teacherId.Count; i++)
+            {
+                 a=Task.teacherId[Task.teacherId.Count - 1];
+                var q = "select TaskName from TaskName where teacher_id=" + a + "";
+                obj.Command(q);
+                obj.TaskNameRead();
+                foreach (var item in Task.taskName)
+                {
+                    TaskBox.Items.Add(item);
+                }
+                break;
+            }
+                     
+        }
+    }
+    class Task
+    {
+        public string connectionString;
+        public string Query;
+        public string TeacherName;
+        public int TeacherId;
+        public string TaskName;
+        public static List<string> myList = new List<string>();
+        public static List<int> teacherId = new List<int>();
+        public static List<string> taskName = new List<string>();
+
+        public SqlConnection Connection(string _connectionString)
+        {
+            connectionString = _connectionString;
+            SqlConnection conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+        public SqlCommand Command(string _Query)
+        {
+            Query = _Query;
+            SqlCommand cmd = new SqlCommand(_Query, Connection(connectionString));
+            cmd.ExecuteNonQuery();
+            return cmd;
+        }
+        public SqlDataReader Reader()
+        {
+            Connection(connectionString);
+            SqlDataReader dataReader = Command(Query).ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read()) {
+                    TeacherName = dataReader.GetString(1);                                 
+                     myList.Add(TeacherName);
+                };
+            }
+            return dataReader;
+        }
+        public SqlDataReader Rdr()
+        {
+            Connection(connectionString);
+            SqlDataReader dataReader = Command(Query).ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                   
+                    TeacherId = dataReader.GetInt32(0);
+                    teacherId.Add(TeacherId);
+                 
+                };
+            }
+            return dataReader;
+        }
+        public SqlDataReader TaskNameRead()
+        {
+            Connection(connectionString);
+            SqlDataReader dataReader = Command(Query).ExecuteReader();
+            if (dataReader.HasRows)
+            {
+                while (dataReader.Read())
+                {
+                    TaskName = dataReader.GetString(0);
+                    taskName.Add(TaskName);
+                };
+            }
+            return dataReader;
+        }
+    }
+}
